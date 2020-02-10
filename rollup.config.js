0ba1c3ed0b93036113721b673d0eaaf4e7069024
Nicholas Peretti <peretti.nicholas@gmail.com>
2020-02-10 15:50:16 +0100

    Refactor TS and Rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,70 +1,94 @@
-import rimraf from "rimraf";
-import typescript from "@rollup/plugin-typescript";
-import babel from "rollup-plugin-babel";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import html from "@rollup/plugin-html";
-import postcss from "rollup-plugin-postcss";
-import tailwind from "tailwindcss";
-import cssnano from "cssnano";
-import purgecss from "@fullhuman/postcss-purgecss";
-import { uglify } from "rollup-plugin-uglify";
-import gzipPlugin from "rollup-plugin-gzip";
+import rimraf from 'rimraf'
 
-import renderHtmlTemplate from "./lib/renderHtmlTemplate";
-import runOnlyInProduction from "./lib/runOnlyInProduction";
-import copyStaticAssets from "./lib/copyStaticAssets";
+//  Rollup plugins
+import typescript from '@rollup/plugin-typescript'
+import babel from 'rollup-plugin-babel'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import postcss from 'rollup-plugin-postcss'
+import tailwind from 'tailwindcss'
+import purgecss from '@fullhuman/postcss-purgecss'
+import cssnano from 'cssnano'
+import { uglify } from 'rollup-plugin-uglify'
+import html from '@rollup/plugin-html'
+import gzipPlugin from 'rollup-plugin-gzip'
 
-const outputDir = "dist";
+//  Custom plugins and helper functions
+import renderHtmlTemplate from './lib/renderHtmlTemplate'
+import copyStaticAssets from './lib/copyStaticAssets'
 
-rimraf.sync(outputDir);
+const outputDir = 'dist'
+rimraf.sync(outputDir)
 
-export default [buildWebApp("src/index.tsx", outputDir)];
+export default [
+  buildWebApp({
+    isProd: process.env.NODE_ENV === 'production',
+  }),
+]
 
-function buildWebApp(inputPath, outputPath) {
+function buildWebApp({ isProd }) {
   return {
-    input: inputPath,
+    input: {
+      index: 'src/index.tsx',
+      sw: 'src/sw/index.ts',
+    },
     output: {
-      dir: outputPath,
-      format: "umd",
-      entryFileNames: "[name]-[hash].js",
-      chunkFileNames: "[name]-[hash].js",
-      assetFileNames: "[name]-[hash].js"
+      dir: outputDir,
+      chunkFileNames: '[name]-[hash].js',
+      chunkFileNames: '[name]-[hash].js',
+      entryFileNames: '[name]-[hash].js',
     },
     plugins: [
-      copyStaticAssets({
-        assets: ["src/assets"]
-      }),
+      //  Parse typescript
       typescript(),
+
+      //  Transpile to ES5
       babel(),
+
+      //  Module loaders
       nodeResolve(),
       commonjs(),
+      json(),
+
+      //  Generate CSS
       postcss({
-        minimize: true,
+        minimize: isProd,
         extract: true,
         plugins: [
+          //  Generate tailwind styles
           tailwind,
-          //  For more info about purgeCSS config check this:
-          //  https://tailwindcss.com/docs/controlling-file-size/
-          ...runOnlyInProduction([
-            purgecss({
-              content: ["./src/**/*.tsx", "./src/*.tsx"],
-              defaultExtractor: content =>
-                content.match(/[\w-/:]+(?<!:)/g) || []
-            }),
-            //  Keep cssnano after purgeCSS to keep the `purgecss ignore` comments
-            cssnano,
-            uglify
-          ])
-        ]
+
+          //  Apply minifiers (prod only)
+          ...(isProd
+            ? [
+                purgecss({
+                  content: ['./src/**/*.tsx', './src/*.tsx'],
+                  defaultExtractor: content =>
+                    content.match(/[\w-/:]+(?<!:)/g) || [],
+                }),
+                cssnano,
+                uglify,
+              ]
+            : []),
+        ],
       }),
+
+      //  Copy assets
+      copyStaticAssets({
+        assets: ['./src/assets/'],
+      }),
+
+      //  Generate index.html
       html({
-        title: "Nicholas Peretti",
+        title: 'Nicholas Peretti',
         template(variables) {
-          return renderHtmlTemplate("src/template.ejs", variables);
-        }
+          return renderHtmlTemplate('src/template.ejs', variables)
+        },
       }),
-      gzipPlugin()
-    ]
-  };
+
+      //  gzip
+      gzipPlugin(),
+    ],
+  }
 }
