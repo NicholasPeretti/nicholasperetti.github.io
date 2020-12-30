88ca6651787977b691b1cdbbd571a190a673d517
Nicholas Peretti <peretti.nicholas@gmail.com>
2020-12-30 23:25:04 +0100

    Fix social meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import SpaceBackground from "../components/SpaceBackground";
-import config from "../config"
-import Head from "next/head"
+import config from "../config";
+import Head from "next/head";
 
-const { socialMeta } = config
+const { socialMeta } = config;
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -12,16 +12,43 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>{socialMeta.title}</title>
         <meta name="description" content={socialMeta.description} />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
-        <meta name="msapplication-TileColor" content="#003447"/>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#003447" />
         <meta name="theme-color" content="#ffffff" />
-        <meta name="image" property="og:image" content="/social-cover.png" />
+
+        {/* Open graph meta */}
+        <meta
+          name="image"
+          property="og:image"
+          content={socialMeta.coverImage}
+        />
         <meta property="og:type" content="Profile" />
+
+        {/* Twitter card meta */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@NicholasPeretti" />
+        <meta name="twitter:title" content={socialMeta.title} />
+        <meta name="twitter:description" content={socialMeta.description} />
+        <meta name="twitter:image" content={socialMeta.coverImage} />
       </Head>
-      <SpaceBackground /> 
+      <SpaceBackground />
       <Component {...pageProps} />
     </>
   );
