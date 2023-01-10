655d2eda065b04e70604e9d126ae5909f8212d6a
Nicholas Peretti <peretti.nicholas@gmail.com>
2023-01-11 00:04:17 +0100

    Redesign

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,137 @@
 import Head from "next/head";
+import Section from "../components/Section";
+import SvgArrowDown from "../components/SvgArrowDown";
+import SvgContact from "../components/SvgContact";
+import SvgExternalLink from "../components/SvgExternalLink";
+import SvgFeedback from "../components/SvgFeedback";
+import SvgFlexible from "../components/SvgFlexible";
+import SvgReferences from "../components/SvgReferences";
+import SvgUxOriented from "../components/SvgUxOriented";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Nicholas Peretti</title>
         <meta name="description" content="Personal website" />
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-    </div>
+      <div className="snap-y snap-mandatory w-screen h-screen overflow-y-scroll">
+        <header className="flex flex-col w-screen justify-between items-center h-screen overflow-x-hidden snap-start">
+          <div className="flex flex-col mt-[150px]">
+            <h1 className="text-[40px] w-fit font-bold bg-gradient-animation mb-[-15px] md:text-[60px] xl:text-[100px]">
+              Nicholas Peretti
+            </h1>
+            <p className="text-[18px] md:text-[30px] xl:text-[50px] font-extralight">
+              Frontend engineer<span className="animate-blink">_</span>
+            </p>
+          </div>
+          <SvgArrowDown className="mb-[50px] w-[50px] animate-bounce" />
+        </header>
+        <Section
+          title="UX oriented"
+          illustration={<SvgUxOriented className="w-full h-full" />}
+        >
+          As a user myself, I really appreciate a good user experience.
+          Therefore I like to work closely with designers from the ideation
+          process, to the mockup finishing touches.
+        </Section>
+        <Section
+          reverse={true}
+          title="Feedback seeker"
+          illustration={<SvgFeedback className="w-full h-full" />}
+        >
+          I feel at home in a company with a strong feedback culture.
+          <br />
+          <br />I regularly seek honest feedback from my peers and I am always
+          available to do my best to reciprocate with constructive comments.
+        </Section>
+        <Section
+          title="Flexible and curious"
+          illustration={<SvgFlexible className="w-full h-full" />}
+        >
+          Even though I am a frontend engineer, I really enjoy exploring all the
+          areas of a product.
+          <br />
+          <br />I appreciate a stimulating environment where I can be exposed to
+          new technologies and ways of working.
+        </Section>
+        <Section
+          reverse={true}
+          title="Work experiences"
+          illustration={<SvgReferences className="w-full h-full" />}
+        >
+          {`I've been working as a software engineer since 2015. I started out as
+          a full-stack developer and I've been focusing on frontend since 2019.`} 
+          <br/><br/>
+          {`You
+          can take a look at my work experience `}
+          <a
+            href="https://www.linkedin.com/in/nicholas-peretti-210aa6130/"
+            target="_blank"
+            rel="noreferrer"
+            className="text-[#F9A826] hover:underline inline-block"
+          >
+            here{" "}
+            <span>
+              <SvgExternalLink className="h-[1em] inline" />
+            </span>
+          </a>
+        </Section>
+        {/* <Section
+          title="Portfolio"
+          illustration={<SvgPortfolio className="w-full" />}
+        >
+          Here are some of my projects
+        </Section> */}
+        <Section
+          // reverse={true}
+          title="Contact me"
+          illustration={<SvgContact className="w-full h-full" />}
+        >
+          <form
+            action="https://api.web3forms.com/submit"
+            method="POST"
+            className="w-full text-[14px] xl:text-[24px] flex flex-col xl:w-[500px]"
+          >
+            <input
+              type="hidden"
+              name="access_key"
+              value="88e0a43e-8858-47dc-961f-a6db6c705901"
+            />
+
+            <label htmlFor="email">
+              Your email:
+              <br />
+              <input
+                required
+                type="email"
+                id="email"
+                name="email"
+                className="w-full mb-[18px]"
+              />
+            </label>
+            <label htmlFor="message">
+              Your message:
+              <br />
+              <textarea
+                required
+                id="message"
+                name="message"
+                className="w-full"
+                rows={5}
+              ></textarea>
+            </label>
+            <button
+              type="submit"
+              className="bg-[#F9A826] h-[60px] text-black font-normal mt-[18px]"
+            >
+              SEND MESSAGE
+            </button>
+          </form>
+        </Section>
+      </div>
+    </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,33 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Archivo Black",
+    },
+    extend: {
+      keyframes: {
+        blink: {
+          "0%": {
+            opacity: 1,
+          },
+          "50%": {
+            opacity: 0,
+          },
+        },
+        "arrow-bounce": {
+          "30%": {
+            opacity: 0.2,
+          },
+          "60%": {
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        blink: "blink 2s linear infinite",
+        bounce: "arrow-bounce 3s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
